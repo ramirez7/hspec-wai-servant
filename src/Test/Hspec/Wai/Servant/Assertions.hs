5b5921f7e23135d4aad926d9fa2fe16a23e78ba4
Isaac Shapira <fresheyeball@gmail.com>
2017-12-13 15:24:40 -0700

    succeed should now print failed responses (#2)
    * succeed should now print failed responses
    
    * don't use  use

--- src/Test/Hspec/Wai/Servant/Assertions.hs
+++ src/Test/Hspec/Wai/Servant/Assertions.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
 
 -- | Assertions for use with the result of the generated client functions
 module Test.Hspec.Wai.Servant.Assertions
@@ -8,8 +9,12 @@ module Test.Hspec.Wai.Servant.Assertions
   , succeed
   ) where
 
+import           Control.Monad.IO.Class       (liftIO)
 import           Data.Functor                 (void)
 import           GHC.Stack                    (HasCallStack)
+import           Network.HTTP.Types.Status    (ok200)
+import           Network.Wai.Test             (SResponse (..))
+import           Test.Hspec.Expectations      (expectationFailure)
 import qualified Test.Hspec.Wai               as W
 
 import           Test.Hspec.Wai.Servant.Types
@@ -31,5 +36,8 @@ shouldRespondWith_ = (void .) . shouldRespondWith
 -- the response
 succeed :: HasCallStack => W.WaiSession (TestResponse a) -> W.WaiSession a
 succeed action = do
-  tresp <- action `shouldRespondWith` 200
-  getTestResponse tresp
+  tresp@(TestResponse _ sres@ (SResponse status _ _)) <- action
+  if status == ok200
+     then getTestResponse tresp
+     else do liftIO . expectationFailure $ "response error:\n  expected: status 200\n  but got: " ++ show sres
+             error "unreachable"
